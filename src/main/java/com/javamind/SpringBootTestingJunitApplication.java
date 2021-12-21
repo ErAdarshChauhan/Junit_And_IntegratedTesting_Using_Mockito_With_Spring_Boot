934cef59d081fc88c7fd18fd548a9a07ed9a90ff
adarsh.chauhan <adarsh.chauhan@optimizeitsystems.com>
2021-12-21 18:29:03 +0530

    adding config to deploy into external tomcat

--- src/main/java/com/javamind/SpringBootTestingJunitApplication.java
+++ src/main/java/com/javamind/SpringBootTestingJunitApplication.java
@@ -2,12 +2,19 @@ package com.javamind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootTestingJunitApplication {
+public class SpringBootTestingJunitApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestingJunitApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootTestingJunitApplication.class);
 	}
 
 }
